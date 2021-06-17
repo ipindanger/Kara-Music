a22a6962db1fcd53466a6958210daf077aa70acb
Irham <74205184+IrhamFadzillah@users.noreply.github.com>
2021-06-18 02:52:56 +0700

    Update play.py

--- rythm/modules/play.py
+++ rythm/modules/play.py
@@ -609,7 +609,7 @@ async def play(_, message: Message):
           await lel.edit("Give me something to play")
         # Looks like hell. Aren't it?? FUCK OFF
         try:
-            toxxt = "****\n\n"
+            toxxt = "****"
             j = 0
             useer=user_name
             emojilist = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣",]
