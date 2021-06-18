24a32ced1e37df392bac8fec85ea38c3ecdfef11
Irham <74205184+IrhamFadzillah@users.noreply.github.com>
2021-06-18 20:43:42 +0700

    Update play.py

--- rythm/modules/play.py
+++ rythm/modules/play.py
@@ -616,9 +616,9 @@ async def play(_, message: Message):
 
             while j < 5:
                 toxxt += f"{emojilist[j]} [Title - {results[j]['title']}](https://youtube.com{results[j]['url_suffix']})\n"
-                toxxt += f" ╚ ⏱️**Duration** - {results[j]['duration']}\n"
-                toxxt += f" ╚ 💡**Views** - {results[j]['views']}\n"
-                toxxt += f" ╚ 🏷️**Channel** - {results[j]['channel']}\n\n"
+                toxxt += f" ╚  ⏱️**Duration** - {results[j]['duration']}\n"
+                toxxt += f" ╚  💡**Views** - {results[j]['views']}\n"
+                toxxt += f" ╚  🏷️**Channel** - {results[j]['channel']}\n\n"
 
                 j += 1            
             koyboard = InlineKeyboardMarkup(
